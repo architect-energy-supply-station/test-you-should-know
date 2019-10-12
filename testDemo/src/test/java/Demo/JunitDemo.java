51b79bb5c5b8d2c88a7cbf46bb0d8968379b4d66
cookr <shuai_0303@163.com>
2019-10-12 10:02:09 +0800

    完成1-5

---
+++ testDemo/src/test/java/Demo/JunitDemo.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitDemo {
+    @Test
+    public void myFirstTest() {
+        //两值是否相等
+        assertEquals(2 + 2, 4);
+    }
+}
